e011b3b728bb9049e3d81a837aa2faca9969a8e9
norbigigakoks <norbeegsoft@gmail.com>
2024-10-13 15:35:17 +0200

    Naprawiono ujemne wartosci poparcia

--- src/PartyForm.jsx
+++ src/PartyForm.jsx
@@ -18,7 +18,7 @@ function PartyForm({initialValue, name, onChange}){
                 <div className="relative flex flex-1 items-center mb-3 ">
                     <input type="number"
                            className="relative px-3 py-3 bg-transparent placeholder:text-slate-500 rounded transition duration-300 ease border border-slate-400 w-full pr-10"
-                           placeholder="0.00" onChange={handleChange} value={popularity}/>
+                           placeholder="0.00" onChange={handleChange} value={popularity} min="0" max="100"/>
                     <span
                         className="absolute right-0 flex items-center px-3 text-blueGray-300 bg-transparent rounded text-base">%</span>
                 </div>
